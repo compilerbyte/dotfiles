d9bf0780012fe509b26fed7d03eb9aa1aa4f0ec5
xhlar <contacto@xhlar.com>
2023-08-09 06:38:32 -0500

    update install.sh with choice to install or not
    Signed-off-by: xhlar <contacto@xhlar.com>

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ red='\e[1;31m'
 white='\e[0;37m'
 dotfiles_repo_dir=$(pwd)
 backup_dir="$HOME/.dotfiles.orig"
-dotfiles_home_dir=(.zsh .aliases .bashrc .exports .functions .zshrc .oh-my-zsh)
+dotfiles_home_dir=(.zsh .aliases .bashrc .exports .functions .zshrc)
 dotfiles_xdg_config_dir=(.kitty .htop .i3 .i3blocks .picom .rofi .espanso)
 
 # Print usage message.
@@ -23,58 +23,65 @@ EOF
 }
 
 install_dotfiles() {
-    # Backup config.
-    if ! [ -f "$backup_dir/check-backup.txt" ]; then
-        mkdir -p "$backup_dir/.config"
-        cd "$backup_dir" || exit
-        touch check-backup.txt
+    read -p "Do you want to install programs and dependencies on your system? (y/n): " choice
+    if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
 
-        # Backup to ~/.dotfiles.orig
-        for dots_home in "${dotfiles_home_dir[@]}"
-        do
-            env cp -rf "$HOME/${dots_home}" "$backup_dir" &> /dev/null
-        done
+      # Backup config.
+      if ! [ -f "$backup_dir/check-backup.txt" ]; then
+          mkdir -p "$backup_dir/.config"
+          cd "$backup_dir" || exit
+          touch check-backup.txt
 
-        # Backup some folder in ~/.config to ~/.dotfiles.orig/.config
-        for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]//./}"
-        do
-            env cp -rf "$HOME/.config/${dots_xdg_conf}" "$backup_dir/.config" &> /dev/null
-        done
+          # Backup to ~/.dotfiles.orig
+          for dots_home in "${dotfiles_home_dir[@]}"
+          do
+              env cp -rf "$HOME/${dots_home}" "$backup_dir" &> /dev/null
+          done
 
-        # Backup again with Git.
-        if [ -x "$(command -v git)" ]; then
-            git init &> /dev/null
-            git add -u &> /dev/null
-            git add . &> /dev/null
-            git commit -m "Backup original config on $(date '+%Y-%m-%d %H:%M')" &> /dev/null
-        fi
+          # Backup some folder in ~/.config to ~/.dotfiles.orig/.config
+          for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]//./}"
+          do
+              env cp -rf "$HOME/.config/${dots_xdg_conf}" "$backup_dir/.config" &> /dev/null
+          done
 
-        # Output.
-        echo -e "${blue}Your config is backed up in ${backup_dir}\n" >&2
-        echo -e "${red}Please do not delete check-backup.txt in .dotfiles.orig folder.${white}" >&2
-        echo -e "It's used to backup and restore your old config.\n" >&2
-    fi
+          # Backup again with Git.
+          if [ -x "$(command -v git)" ]; then
+              git init &> /dev/null
+              git add -u &> /dev/null
+              git add . &> /dev/null
+              git commit -m "Backup original config on $(date '+%Y-%m-%d %H:%M')" &> /dev/null
+          fi
 
-    # Install config.
-    for dots_home in "${dotfiles_home_dir[@]}"
-    do
-        env rm -rf "$HOME/${dots_home}"
-        env ln -fs "$dotfiles_repo_dir/${dots_home}" "$HOME/"
-    done
+          # Output.
+          echo -e "${blue}Your config is backed up in ${backup_dir}\n" >&2
+          echo -e "${red}Please do not delete check-backup.txt in .dotfiles.orig folder.${white}" >&2
+          echo -e "It's used to backup and restore your old config.\n" >&2
+      fi
 
-    # Install config to ~/.config.
-    mkdir -p "$HOME/.config"
-    for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]}"
-    do
-        env rm -rf "$HOME/.config/${dots_xdg_conf[*]//./}"
-        env ln -fs "$dotfiles_repo_dir/${dots_xdg_conf}" "$HOME/.config/${dots_xdg_conf[*]//./}"
-    done
+      # Install config.
+      for dots_home in "${dotfiles_home_dir[@]}"
+      do
+          env rm -rf "$HOME/${dots_home}"
+          env ln -fs "$dotfiles_repo_dir/${dots_home}" "$HOME/"
+      done
 
-    echo -e "${blue}New dotfiles is installed!\n${white}" >&2
-    echo "There may be some errors when Terminal is restarted." >&2
-    echo "Please read carefully the error messages and make sure all packages are installed. See more info in README.md." >&2
-    echo "Note that the author of this dotfiles uses dev branch in some packages." >&2
-    echo -e "If you want to restore your old config, you can use ${red}./install.sh -r${white} command." >&2
+      # Install config to ~/.config.
+      mkdir -p "$HOME/.config"
+      for dots_xdg_conf in "${dotfiles_xdg_config_dir[@]}"
+      do
+          env rm -rf "$HOME/.config/${dots_xdg_conf[*]//./}"
+          env ln -fs "$dotfiles_repo_dir/${dots_xdg_conf}" "$HOME/.config/${dots_xdg_conf[*]//./}"
+      done
+
+      echo -e "${blue}New dotfiles is installed!\n${white}" >&2
+      echo "There may be some errors when Terminal is restarted." >&2
+      echo "Please read carefully the error messages and make sure all packages are installed. See more info in README.md." >&2
+      echo "Note that the author of this dotfiles uses dev branch in some packages." >&2
+      echo -e "If you want to restore your old config, you can use ${red}./install.sh -r${white} command." >&2
+  else
+    echo "Cancel installation of dotfiles"
+    exit
+  fi
 }
 
 install_programs(){
@@ -85,7 +92,7 @@ install_programs(){
       chmod +x programs.sh
       ./programs.sh -i
   else
-      echo "Welcome back sir. Cancel installation."
+      echo "Cancel installation of programs."
   fi 
 
 }
