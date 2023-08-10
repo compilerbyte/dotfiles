46c000c765be51a2b8fc410dfbc71a37d6c385d9
xhlar <contacto@xhlar.com>
2023-08-10 08:06:00 -0500

    adding: rofi configuration to category apps & remove: configuration to .ranger
    Signed-off-by: xhlar <contacto@xhlar.com>

--- .rofi/rofi_apps.sh
+++ .rofi/rofi_apps.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-selected_category=$(echo -e "web_apps\noffice_apps\nmedia_apps" | rofi -dmenu -p "Select category")
+selected_category=$(echo -e "web_apps\noffice_apps\nmedia_apps\ngraphics_apps" | rofi -dmenu -p "Select category")
 
 case "$selected_category" in
     web_apps)
-        apps=$(cat ~/.config/rofi/web_apps.txt)
+        apps_dir=~/.config/rofi/web_apps
         ;;
     office_apps)
-        apps=$(cat ~/.config/rofi/office_apps.txt)
+        apps_dir=~/.config/rofi/office_apps
         ;;
     media_apps)
-        apps=$(cat ~/.config/rofi/media_apps.txt)
+        apps_dir=~/.config/rofi/media_apps
+        ;;
+    graphics_apps)
+        apps_dir=~/.config/rofi/graphics_apps
         ;;
     *)
         exit 1
         ;;
 esac
 
-chosen_app=$(echo "$apps" | rofi -dmenu -p "Select app")
+chosen_app=$(ls "$apps_dir" | sed 's/\.sh$//' | rofi -dmenu -p "Select app")
 
 if [ -n "$chosen_app" ]; then
-    "$chosen_app" &
+    sh "$apps_dir/$chosen_app.sh" &
 fi
 
